07fe6e532a889a734f61d1b7da44fb264329c6ef
MNAG19 <naufal.anugrah@mhs.mdp.ac.id>
2022-09-22 15:10:28 +0700

    gmap

--- app/src/main/java/com/mdp/if3a/peta/MainActivity.java
+++ app/src/main/java/com/mdp/if3a/peta/MainActivity.java
@@ -1,14 +1,42 @@
 package com.mdp.if3a.peta;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.mdp.if3a.peta.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private ActivityMainBinding binding;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+//        mMap.setMyLocationEnabled(true);
+        LatLng latLngUser = new LatLng(-2.983815, 104.751879);
+        mMap.addMarker(new MarkerOptions().position(latLngUser).title("Lokasi Saya"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLngUser, 19 ));
     }
 }
